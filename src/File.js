1ea1507414ebdeb404401134627f92571cc19abf
connuh <cnriscute@gmail.com>
2021-04-16 18:57:23 +0000

    🎉 * Update File.js `save()`

--- src/File.js
+++ src/File.js
@@ -9,11 +9,11 @@ const fs = require("fs");
 // File Class
 class File {
   /**
-   * @param {String} dir 
+   * @param {String} path 
    */
-  static load(dir) {
+  static load(path) {
     return new Promise((resolve, reject) => {
-      let data = "", stream = fs.createReadStream(dir);
+      let data = "", stream = fs.createReadStream(path);
 
       stream.on("data", d => data += d.toString());
   
@@ -25,6 +25,19 @@ class File {
         return reject(e);
       });
     });
+  }
+
+  /**
+   * @param {String} path 
+   * @param {String} data 
+   */
+  static save(path, data) {
+    try {
+      fs.appendFileSync(path, `${data}\n`);
+      return true;
+    } catch {
+      return false;
+    }
   }
 }
 
