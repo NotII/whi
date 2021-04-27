578933967b72b4e01f4aa01130528fc265842b72
connuh <cnriscute@gmail.com>
2021-04-27 21:32:49 +0000

    🎉 + Add `Checker.js` & `Scraper.js`

---
+++ src/tools/Checker.js
@@ -0,0 +1,12 @@
+/**
+ * @author Conner
+ * @since 27/04/21
+ */
+
+// Checker Class
+class Checker {
+  // TODO: Figure out how we will implement this
+  // ^ possibly using `EventEmitter`?
+}
+
+module.exports = Checker;
---
+++ src/tools/Scraper.js
@@ -0,0 +1,17 @@
+/**
+ * @author Conner
+ * @since 27/04/21
+ */
+
+// Scraper Class
+class Scraper {
+  constructor(user) {
+    this.user = user;
+  }
+
+  scrape() {
+
+  }
+}
+
+module.exports = Scraper;
