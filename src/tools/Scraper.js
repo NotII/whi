24617427186954fdbc08a3e84eaa60b8c884db34
connuh <cnriscute@gmail.com>
2021-04-27 21:59:43 +0000

    🎉 * Add scraping functionality!

--- src/tools/Scraper.js
+++ src/tools/Scraper.js
@@ -3,14 +3,58 @@
  * @since 27/04/21
  */
 
+// Dependencies
+const { EventEmitter } = require("events");
+
+// Utils
+const Image = require("../whi/Image");
+
 // Scraper Class
-class Scraper {
-  constructor(user) {
+class Scraper extends EventEmitter {
+  /**
+   * @param {String} user 
+   * @param {String|Number} page 
+   * @param {String|Number} pages 
+   */
+  constructor(user, page = 0, pages = 1000) {
+    super();
+
     this.user = user;
+    this.page = page;
+    this.end = (page + pages) > 1000 ? 1000 : page + pages;
+
+    this.scraping = false;
   }
 
-  scrape() {
+  async stop() {
+    this.scraping = false;
 
+    this.emit("finished");
+  }
+
+  async scrape() {
+    this.emit("scraping", {
+      user: this.user,
+      page: this.page,
+      end: this.end
+    });
+
+    this.scraping = true;
+
+    while(this.scraping) {
+      this.emit("images", {
+        images: await(Image.getImages(this.user, this.page)),
+        page: this.page
+      });
+
+      if(this.page === this.end) {
+        this.emit("finished");
+
+        this.scraping = false;
+      }
+
+      this.page++;
+    }
   }
 }
 
