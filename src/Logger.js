0965c6e487e83b2247b1f67b81f8a1d7fb337851
connuh <cnriscute@gmail.com>
2021-04-28 05:56:53 +0000

    🎉 + Add colours / prefix to `src/Logger.js`

--- src/Logger.js
+++ src/Logger.js
@@ -6,9 +6,9 @@
 // Logger Class
 class Logger {
   // If you think I am adding JSDoc for some strings, you're mistaken.
-  static info = string => console.log(`[info] ${string}`);
-  static warn = string => console.log(`[warn] ${string}`);
-  static error = string => console.log(`[error] ${string}`);
+  static info = (prefix, string) => console.log(`\x1b[94m[${prefix} | info]\x1b[37m ${string}`);
+  static warn = (prefix, string) => console.log(`\x1b[93m[${prefix} | warn]\x1b[37m ${string}`);
+  static error = (prefix, string) => console.log(`\x1b[91m[${prefix} | error]\x1b[37m ${string}`);
 }
 
 module.exports = Logger;
