7b703509da1fa671a583d84ac81dac56f9d56bde
connuh <cnriscute@gmail.com>
2021-04-28 06:38:52 +0000

    🎉 * Document `src/tools/Scraper.js`

--- src/tools/Scraper.js
+++ src/tools/Scraper.js
@@ -17,46 +17,96 @@ class Scraper extends EventEmitter {
    * @param {String|Number} pages 
    */
   constructor(user, page = 0, pages = 1000) {
+    /**
+     * We call `super()` so we can access functions
+     * from `EventEmitter`
+     */
     super();
 
+    /**
+     * Setup variables from the constructor
+     */
     this.user = user;
     this.page = page;
     this.end = (page + pages) > 1000 ? 1000 : page + pages;
 
+    /**
+     * Set `this.scraping` to false by default.
+     */
     this.scraping = false;
   }
 
   async stop() {
+    /**
+     * Set `this.scraping` to false so the while() in `scrape()`
+     * stops running.
+     */
     this.scraping = false;
 
+    /**
+     * Emit the `finished` event so the user knows we ended scraping!
+     */
     this.emit("finished");
   }
 
   async scrape() {
+    /**
+     * Emit the `scraping` event with the variables
+     * from the constructor so we can use in `main.js`
+     */
     this.emit("scraping", {
       user: this.user,
       page: this.page,
       end: this.end
     });
 
+    /**
+     * Set `this.scraping` to true for the
+     * while() loop!
+     */
     this.scraping = true;
 
+    /**
+     * While `this.scraping` is true
+     * we will run all code in the scope
+     */
     while(this.scraping) {
+      /**
+       * Call `try` here incase the `await`
+       * throws an error!
+       */
       try {
+        /**
+         * Emit `images` with an array containing
+         * image URLs and the current page as `page`
+         */
         this.emit("images", {
           images: await(Image.getImages(this.user, this.page)),
           page: this.page
         });
       } catch(e) {
+        /**
+         * If `Image.getImages()` throws
+         * an error, we will emit it!
+         */
         this.emit("error", e);
       }
 
+      /**
+       * If `this.page` is equal to `this.end`
+       * we will emit `finished` and set
+       * `this.scraping` to false to end the while
+       */
       if(this.page === this.end) {
         this.emit("finished");
 
         this.scraping = false;
       }
 
+      /**
+       * Increment the current page every
+       * time we emit some images!
+       */
       this.page++;
     }
   }
